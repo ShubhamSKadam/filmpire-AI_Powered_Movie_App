28a0c1ecdcb6d606a46256be6d41e9d9272dfcf4
Shubham Kadam <shubhamskadam99@outlook.com>
2023-01-09 14:06:14 +0530

    updated npm package for mui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { CssBaseline } from "@mui/material";
+// import { Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <div>
+            <CssBaseline />
+            <h1>Hello, World - Filmpire</h1>
+        </div>
+    );
+};
 
-export default App
+export default App;
