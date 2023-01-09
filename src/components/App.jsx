971a878a03acd63f080348c3f18245c875cee16e
Shubham Kadam <shubhamskadam99@outlook.com>
2023-01-09 14:41:57 +0530

    react-dom downgraded to fix Switch error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
-// import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 const App = () => {
     return (
         <div>
             <CssBaseline />
-            <h1>Hello, World - Filmpire</h1>
+            <main>
+                <Switch>
+                    <Route exact path="/movie/:id">
+                        <h1>Movie Information</h1>
+                    </Route>
+                    <Route exact path="/actors/:id">
+                        <h1>Actors</h1>
+                    </Route>
+                    <Route exact path="/">
+                        <h1>Movies</h1>
+                    </Route>
+                    <Route exact path="/profile/:id">
+                        <h1>Profile</h1>
+                    </Route>
+                </Switch>
+            </main>
         </div>
     );
 };
