3ef2b566b59ad08129a64c22bac4d54b65b0d4fd
Shubham Kadam <shubhamskadam99@outlook.com>
2023-01-10 18:51:20 +0530

    Working Over Sidebar and Categories

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     AppBar,
     IconButton,
@@ -20,7 +20,10 @@ import { useTheme } from "@mui/material/styles";
 import useStyles from "./styles";
 import { Link } from "react-router-dom";
 
+import { Sidebar } from "..";
+
 const NavBar = () => {
+    const [mobileOpen, setMobileOpen] = useState(false);
     const classes = useStyles();
     const isMobile = useMediaQuery("(max-width:600px)");
     const theme = useTheme();
@@ -77,6 +80,29 @@ const NavBar = () => {
                     {isMobile && "Search..."}
                 </Toolbar>
             </AppBar>
+            <div>
+                <nav className={classes.drawer}>
+                    {isMobile ? (
+                        <Drawer
+                            variant="temporary"
+                            anchor="right"
+                            open={mobileOpen}
+                            classes={{ paper: classes.drawerPaper }}
+                            ModalProps={{ keepMounted: true }}
+                        >
+                            <Sidebar setMobileOpen={setMobileOpen} />
+                        </Drawer>
+                    ) : (
+                        <Drawer
+                            classes={{ paper: classes.drawerPaper }}
+                            variant="permanent"
+                            open
+                        >
+                            <Sidebar setMobileOpen={setMobileOpen} />
+                        </Drawer>
+                    )}
+                </nav>
+            </div>
         </>
     );
 };
--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,5 +1,7 @@
 import { makeStyles } from "@mui/styles";
 
+const drawerWidth = 240;
+
 export default makeStyles((theme) => ({
     toolbar: {
         height: "80px",
@@ -7,7 +9,7 @@ export default makeStyles((theme) => ({
         justifyContent: "space-between",
         marginLeft: "240px",
         [theme.breakpoints.down("sm")]: {
-            marginLeft:0,
+            marginLeft: 0,
             flexWrap: "wrap",
         },
     },
@@ -17,4 +19,20 @@ export default makeStyles((theme) => ({
             display: "none",
         },
     },
+
+    drawer: {
+        [theme.breakpoints.up("sm")]: {
+            width:drawerWidth,
+            flexShrink:0,
+        },
+    },
+    drawerPaper : {
+            width: drawerWidth,
+    },
+    linkButton:{
+        '&:hover':{
+            color:'while !important',
+            textDecoration : 'none',
+        }
+    }
 }));
