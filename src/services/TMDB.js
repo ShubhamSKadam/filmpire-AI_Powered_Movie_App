0814f0e63af42668a706e3d28db04dfe440b185b
Shubham Kadam <shubhamskadam99@outlook.com>
2023-01-12 16:20:49 +0530

    Completed Redux Setup

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const tmdbApiKey = process.env.TMDB_KEY;
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
 // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 const page = 1;
 export const tmdbApi = createApi({
