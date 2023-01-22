e0814dd492205554eb655d11cf142131ae786ab4
Shubham Kadam <shubhamskadam99@outlook.com>
2023-01-22 09:47:06 +0530

    Completing Authentication

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
+import { userSelector } from "../../features/auth";
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+    const { user } = useSelector(userSelector);
+    console.log(user);
+    return <div>Profile - {user.username}</div>;
+};
 
-export default Profile
+export default Profile;
