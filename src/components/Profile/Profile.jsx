48fad55bd93ef33d5f17f8476656ebcc2e5b1654
Shubham Kadam <shubhamskadam99@outlook.com>
2023-01-22 11:04:06 +0530

    Slight fix in authentication

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Typography, Button, Box } from "@mui/material";
 import { useSelector } from "react-redux";
+import { ExitToApp, Face } from "@mui/icons-material";
 
 import { userSelector } from "../../features/auth";
 const Profile = () => {
     const { user } = useSelector(userSelector);
-    console.log(user);
-    return <div>Profile - {user.username}</div>;
+
+    const favoriteMovies = [];
+    const logout = () => {
+        localStorage.clear();
+        window.location.href = "/";
+    };
+    return (
+        <div>
+            <Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="h4" gutterBottom>
+                        My Profile
+                    </Typography>
+                    <Button color="inherit" onClick={logout}>
+                        Logout &nbsp; <ExitToApp />
+                    </Button>
+                </Box>
+                {!favoriteMovies.length ? (
+                    <Typography variant="h5">
+                        Add favorites or watchlist some movies to see them here!
+                    </Typography>
+                ) : (
+                    <Box>favoriteMovies</Box>
+                )}
+            </Box>
+        </div>
+    );
 };
 
 export default Profile;
