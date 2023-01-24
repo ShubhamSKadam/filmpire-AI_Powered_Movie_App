dc013d757fedd89cc9cae8a251ea9dfd2d76ec91
Shubham Kadam <shubhamskadam99@outlook.com>
2023-01-24 22:18:20 +0530

    Movie information - Title,Subtitle,GenreIcons

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -26,11 +26,14 @@ import axios from "axios";
 
 import { useGetMovieQuery } from "../../services/TMDB";
 import useStyles from "./styles";
+import genreIcons from "../../assets/genres";
+import { selectGenreOrCategory } from "../../features/currentGenreOrCategory";
 
 const MovieInformation = () => {
     const { id } = useParams();
     const { data, isFetching, error } = useGetMovieQuery(id);
     const classes = useStyles();
+    const dispatch = useDispatch();
 
     if (isFetching) {
         return (
@@ -55,6 +58,50 @@ const MovieInformation = () => {
                     src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
                     alt={data?.title}
                 />
+            </Grid>
+            <Grid item container direction="column" lg={7}>
+                <Typography variant="h3" align="center" gutterBottom>
+                    {data?.title} {data.release_date.split("-")[0]}
+                </Typography>
+                <Typography variant="h5" align="center" gutterBottom>
+                    {data?.tagline}
+                </Typography>
+                <Grid item className={classes.containerSpaceAround}>
+                    <Box display="flex" align="center">
+                        <Rating readOnly value={data.vote_average / 2} />
+                        <Typography
+                            variant="subtitle1"
+                            gutterBottom
+                            style={{ marginLeft: "10px" }}
+                        >
+                            {data?.vote_average}/10
+                        </Typography>
+                    </Box>
+                    <Typography variant="h6" align="center" gutterBottom>
+                        {data?.runtime}min{" "}
+                        {data?.spoken_languages.length > 0
+                            ? `/ ${data?.spoken_languages[0].name}`
+                            : ""}
+                    </Typography>
+                </Grid>
+                <Grid item className={classes.genresContainer}>
+                    {data?.genres?.map((genre) => (
+                        <Link
+                            key={genre.name}
+                            className={classes.links}
+                            to="/"
+                            onClick={() => dispatch(selectGenreOrCategory(genre.id))}>
+                            <img
+                                src={genreIcons[genre.name.toLowerCase()]}
+                                className={classes.genreImage}
+                                height={30}
+                            />
+                            <Typography color="textPrimary" variant="subtitle1">
+                                {genre?.name}
+                            </Typography>
+                        </Link>
+                    ))}
+                </Grid>
             </Grid>
         </Grid>
     );
--- src/components/MovieInformation/styles.js
+++ src/components/MovieInformation/styles.js
@@ -10,4 +10,39 @@ export default makeStyles((theme) => ({
             flexWrap: "wrap",
         },
     },
+    poster: {
+        borderRadius: "20px",
+        boxShadow: "0.5em 1em 1em rgb(64,64,70)",
+        width: "80%",
+            [theme.breakpoints.down("md")]: {
+            margin: "0 auto",
+            width: "50%",
+            height: "350px",
+        },
+        [theme.breakpoints.down("sm")]: {
+            margin: "0 auto",
+            width: "100%",
+            height: "350px",
+            marginBottom: "30px",
+        },
+    },
+    genresContainer:{
+      margin:'10px 0 !important',
+      display:'flex',
+      justifyContent:'space-around',
+      flexWrap:'wrap',
+    },
+    genreImage:{
+      filter: theme.palette.mode==='dark'&& 'invert(1)',
+      marginRight:'10px',
+    },
+    links:{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+       [theme.breakpoints.down("sm")]: {
+            padding:'0.5rem 1rem',
+
+        },
+    }
 }));
