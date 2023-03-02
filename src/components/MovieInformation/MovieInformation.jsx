1dbfc172967e80a96fd4e9983f44f93feaac1d18
alok verma <alokverma749@gmail.com>
2023-03-02 22:50:14 +0530

    repalce toFixed with Math.round()

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -146,7 +146,7 @@ const MovieInformation = () => {
                             gutterBottom
                             style={{ marginLeft: "10px" }}
                         >
-                            {(data?.vote_average).toFixed(1)}/ 10
+                            {Math.round(data.vote_average * Math.pow(10, 1)) / Math.pow(10, 1)}/10
                         </Typography>
                     </Box>
                     <Typography variant="h6" align="center" gutterBottom>
